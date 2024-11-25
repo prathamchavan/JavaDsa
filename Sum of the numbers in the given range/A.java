7e1531d185122ba423a6f62c95b136de1e909c30
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-11-25 22:31:35 +0530

    commit

---
+++ Sum of the numbers in the given range/A.java
@@ -0,0 +1,16 @@
+
+
+public class A {
+  public static void main(String args[]){
+
+    int num1 = 5;
+    int num2 = 10;
+    int sum = 0;
+
+    for(int i = num1; i <= num2; i++){
+      sum = sum + i;
+    }
+
+    System.out.println(sum);
+  }
+}
