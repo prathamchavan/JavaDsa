f89cc0f016cc0b54648d7addb15e07fdce4d2c7c
Prathamesh Chavan <prathamchavan0013@gmail.com>
2025-02-10 15:52:45 +0530

    arraylist

---
+++ Collection Framework/ALP.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class ALP
+{
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    //adding elements to list 
+    list.add("Ravi");
+    list.add("vijay");
+    list.add("Ravi");
+    list.add("Ajay");
+
+    System.out.println(list);
+  
+  }
+}
